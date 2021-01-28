0cb6aaf5c652590896d5cda6bc2cecb26e7c22d3
tarsila <tarsilaterezaprm@gmail.com>
2021-01-28 20:43:40 -0300

    Aprendendo
    Criei outra pasta de JS. Exercícios usando Template String e Place Holder (não sei se escrevi certo)

---
+++ aulas/aula 2/ex/ex.js
@@ -0,0 +1,5 @@
+var nome = prompt('Qual é seu nome?')
+document.write(`Olá ${nome}, seu nome tem ${nome.length} letras`)
+document.write(`teu nome maiusculo é ${nome.toUpperCase()} `)
+
+document.write(` teu nome em minúsculo é ${nome.toLowerCase()}`)
--- aulas/aula 2/script.js
+++ aulas/aula 2/script.js
@@ -6,5 +6,14 @@
 var n1 = Number(window.prompt('Digite um número'))
 var n2 = Number(window.prompt('Outro número'))
 var soma = n1 + n2
-console.log('a soma dos valores é ' + soma)
+console.log(`a soma dos valores é ${soma}`)
 alert( 'a soma é ' + soma)
+
+var resposta1 = String(prompt('Digite um número'))
+var resposta2 = String(prompt('Digite mais um núumero'))
+var juncao = resposta1 + resposta2
+
+alert(`A junção desses números é ${juncao}`)
+
+
+
