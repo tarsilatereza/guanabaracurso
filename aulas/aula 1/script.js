8e1c6738f14428ae9a78207c4d8e4e9f2756d9b5
tarsila <tarsilaterezaprm@gmail.com>
2021-01-28 18:05:34 -0300

    Testes
    Estruturas básicas de JavaScript para relembrar e praticar.

---
+++ aulas/aula 1/script.js
@@ -0,0 +1,8 @@
+// window.alert('Mensagem de alerta') 
+// Esse window pode ser tirado
+
+// window.confirm('Meu JS está funcionando?') 
+// window.prompt('Digite seu nome') //espaço para responder
+
+var nome = window.prompt('Digite seu nome') // O RESULTADO DO PROMPT VAI FICAR NA VARIÁVEL NOME
+alert('Opa! prazer em te conhecer, ' + nome)  // CONCATENAÇÃO
---
+++ aulas/aula 2/script.js
@@ -0,0 +1,10 @@
+// var n1 = Number.parseInt(window.prompt('Digite um número'))
+// var n2 = Number.parseInt(window.prompt('Outro número'))
+// var soma = n1 + n2
+// console.log('a soma dos valores é ' + soma)
+// alert( 'a soma é ' + soma)
+var n1 = Number(window.prompt('Digite um número'))
+var n2 = Number(window.prompt('Outro número'))
+var soma = n1 + n2
+console.log('a soma dos valores é ' + soma)
+alert( 'a soma é ' + soma)
