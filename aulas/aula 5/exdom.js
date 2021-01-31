c85698754d9fe9a3cb05bce8f9534a0877905232
tarsila <tarsilaterezaprm@gmail.com>
2021-01-30 21:02:30 -0300

    Eventos
    Adicionando evento na página com JavaScript parte 1. Usei addEventListener e o método de pegar direito do HTML.

--- aulas/aula 5/exdom.js
+++ aulas/aula 5/exdom.js
@@ -1,13 +1,18 @@
-var a = window.document.getElementById('area')
+// var a = window.document.getElementById('area')
+// a.addEventListener('click', clicar)
+// a.addEventListener('mouseenter', entrar)
+// a.addEventListener('mouseout', sair)
 
-function clicar() {
-    a.innerText = 'Clicou!'
-}
+// function clicar() {
+//     a.innerText = 'Clicou!'
+//     a.style.background = "red"
+// }
 
-function entrar() {
-   a.innerText = 'Entrando...'
-}
+// function entrar() {
+//    a.innerText = 'Entrando...'
+// }
 
-function sair() {
-    a.innerText = "Saindo..."
-}
+// function sair() {
+//     a.innerText = "Saindo..."
+//     a.style.background = "lightslategrey"
+// }
