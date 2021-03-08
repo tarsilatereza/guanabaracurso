d6b183a3d9c5de086a00452f00fabafa5991306e
tarsila <74773587+TarsilaTereza@users.noreply.github.com>
2021-03-08 17:49:30 -0300

    Css e JS :)
    Editando atributos no JS

--- aulas/exercício1/script.js
+++ aulas/exercício1/script.js
@@ -1,27 +1,41 @@
-
-function carregar (){
+function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-     
-    if(hora > 18) {
-     msg.innerHTML = `Agora são ${hora} horas, boa noite.`  
-       
-     img.src = 'night.jpg'   
+    
+    var atributo = document.getElementById('imagem')
+    var alinhamento = document.querySelector('div#msg')
+    
+    if (hora > 18) {
+        msg.innerHTML = `Agora são ${hora} horas, boa noite.`
 
-    } else if (hora > 12){
+        img.src = "./images/night.png"
+        atributo.style.width = '70%'
+        atributo.style.height = 'auto'
+
+    } else if (hora > 12) {
         msg.innerHTML = `Agora são ${hora} horas, boa tarde`
-      
-        img.src = 'afternoon.jpg'
 
-    } else if (hora > 5){
+        img.src = "./images/afternoon.png"
+        atributo.style.width = '70%'
+        atributo.style.height = 'auto'
+
+    } else if (hora > 5) {
         msg.innerHTML = `Agora são ${hora} horas, bom dia`
-        
-        img.src = 'morning.jpg'
-        
-    } else{
+
+        img.src = "./images/morning.png"
+        atributo.style.width = 'auto'
+        atributo.style.height = '333px'
+
+    } else {
         msg.innerHTML = `Agora são ${hora} horas, boa madrugada. Vai dormir maluco`
+        img.src = "./images/dawn.png"
+
+        atributo.style.width = '70%'
+        atributo.style.height = 'auto'
     }
 
 }
+
+
