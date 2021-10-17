58b2d9871c713990b29207e91bf778393c2d3ed5
tarsila <tarsilaterezaprm@gmail.com>
2021-10-17 16:29:43 -0300

    tabuada de multiplicação simples

--- aulas/exercicio5/script.js
+++ aulas/exercicio5/script.js
@@ -1,16 +1,27 @@
-function gerar() {
-    let number = document.getElementById('number')
-    let button = document.querySelector('input#button')
-    var res = document.querySelector('div#result')
-    
+    var res = document.querySelector('div#result');
+
+    function gerar() {
+        var number = document.getElementById('number');
+        let button = document.querySelector('input#button');
 
 
-    if (number.value.length == 0) {
-        alert("ERRO. PREENCHA O CAMPO")
 
-    } else {
-        for (var x = 1; x <= 10; x++) {
-                res.innerHTML += `${x*number.value}`
+        if (number.value.length == 0) {
+            alert("ERRO. PREENCHA O CAMPO");
+
+        } else {
+            for (var x = 1; x <= 10; x++) {
+                let multi = x*number.value;
+                res.innerHTML += `${x} * ${number.value} = ${multi}` + '<br>';
+            }
         }
     }
+
+    function clearBox (){
+
+        location.reload();
+
+    /* ou
+    res.innerHTML = "" pra só limpar o resultado*/
+
 }
