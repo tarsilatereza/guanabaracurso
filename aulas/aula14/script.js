ad5202bad3e7d1ea19de5ca51ab43080b151fe59
tarsila <tarsilaterezaprm@gmail.com>
2021-11-04 16:56:31 -0300

    function

---
+++ aulas/aula14/script.js
@@ -0,0 +1,31 @@
+/*function parimpar(n){
+	if (n%2 == 0){
+		return "par"
+	} else {
+		return "impar"
+	}
+}
+
+console.log(parimpar(5))*/
+
+function soma (a, b){
+	return a + b
+}
+
+console.log(soma(1, 2));
+
+// if the a or b is not define:
+
+function somaIndefinida (x = 0, y = 0){  //já começa sendo 0
+	return x + y
+}
+
+console.log(somaIndefinida(7)) // só foi passado um parâmetro, portanto: 7 + 0 = 7
+
+
+//COLOCAR FUNÇÃO EM UMA VARIÁVEL
+
+var z = function(w){
+	return 2*w
+} 
+console.log(z(5))
