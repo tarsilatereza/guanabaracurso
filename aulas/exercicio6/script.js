a823e96389eb13169b09ae730270b5d1c5888143
tarsila <tarsilaterezaprm@gmail.com>
2021-11-05 11:19:30 -0300

    estrutura do html, começando o script

--- aulas/exercicio6/script.js
+++ aulas/exercicio6/script.js
@@ -1,1 +1,11 @@
+var number = document.querySelector('input#number')
+var area = document.querySelector('select#areaSelect')
+var finish = document.querySelector('input#finishButton')
 
+function add(){
+	if (number.value == 0){
+		alert("escolha um número de 1 a 100")
+	} else {
+		area.innerHTML = `Valor ${number.value} foi adicionado`
+	}
+}
