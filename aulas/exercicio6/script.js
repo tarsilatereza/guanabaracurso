461588a9fea1460a55453ec0c74dd1e61772be9e
tarsila <tarsilaterezaprm@gmail.com>
2021-11-06 12:26:02 -0300

    criação de option

--- aulas/exercicio6/script.js
+++ aulas/exercicio6/script.js
@@ -2,10 +2,27 @@ var number = document.querySelector('input#number')
 var area = document.querySelector('select#areaSelect')
 var finish = document.querySelector('input#finishButton')
 
+
 function add(){
-	if (number.value == 0){
+	if (number.value < 1 || number.value > 100){
 		alert("escolha um número de 1 a 100")
 	} else {
-		area.innerHTML = `Valor ${number.value} foi adicionado`
+		var selectOption = document.createElement('option')
+		var option = document.getElementsByName('option')
+		selectOption.text += `Número ${number.value} adicionado`
+		area.appendChild(selectOption)
+
+	}
+
+}
+
+function result(){
+	if (number.value < 1 || number.value > 100){
+		alert("escolha pelo menos um número de 1 a 100")
+	} else {
+		let result = document.getElementById('resultado')
+		
+		// var list = option.value
+		// alert(list)
 	}
 }
